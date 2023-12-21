dc71fb0f321e284d3e367fe7709c49fb194d2892
Padma Patil <abdrazakovermek9@gmail.com>
2023-12-21 08:02:52 -0800

    Fix img files

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../src/css/styles.css';
 //import '...' from './js/projectName.js';
-import './assets/images/stock-puppy-photo.jpg';
+
